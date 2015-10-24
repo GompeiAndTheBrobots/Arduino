4c015823377145acbafbd976d35a951c3902bacc
engineeringawesome <robomower9508@gmail.com>
2015-10-24 16:29:06 -0400

    Reads button states and sends them

--- SampleField/src/sketch.ino
+++ SampleField/src/sketch.ino
@@ -1,8 +1,21 @@
 void setup(){
+	//Start the serial communications
 	Serial.begin(115200);
+
+	//Initialize all the inputs for the buttons
+	for(int i=0; i<8; i++) {
+		pinMode(i, INPUT_PULLUP);
+	}
 }
 
 void loop(){
-	Serial.write(0b10100101);
-	delay(250);
+	uint8_t availabilityByte = 0;
+	//Fill the bitfield
+	for(int i=0; i<8; i++) {
+		availabilityByte |= (digitalRead(i) << i);
+	}
+	//Send the bitfield
+	Serial.write(availabilityByte);
+	//Only update every so often
+	delay(500);
 }
